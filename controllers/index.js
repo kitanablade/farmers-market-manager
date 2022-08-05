b92315f402d2bbfbac095a054331f64cf8189002
Lesli <lpatino0994@gmail.com>
2022-08-05 14:07:06 -0500

    finished basic homepage structure

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 const apiRoutes = require("./api");
 
+//renders homepage
+router.get('/',(req,res)=>{
+    res.render("home")
+})
+
+//renders vendor page
+// router.get('/vendor', (req, res)=>{
+//     res.render("vendor")
+// })
 
 
 
-
-
+router.use("/api",apiRoutes)
 
 module.exports = router;
