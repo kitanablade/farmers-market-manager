658d791469711a8816970c16af6891494a36c58c
Lesli <lpatino0994@gmail.com>
2022-08-07 01:46:48 -0500

    added all handlebars get routes and handlebar files for each route

--- controllers/index.js
+++ controllers/index.js
@@ -4,21 +4,10 @@ const apiRoutes = require("./api");
 const handlebrs = require('express-handlebars');
 const {Event, Product, Vendor}= require('../models');
 
-//renders events
-router.get('/',(req,res)=>{
-    Event.findAll({
-    }).then(data=>{
-        const hbsData = data.map(modelIns=>modelIns.toJSON())
-        res.render("home",{
-            events:hbsData
-        })
-    })
-
-})
 
 //log in route
 router.get("/login",(req,res)=>{
-    res.render("login")             // Don't think we need this
+    res.render("login")             // Don't think we need this BECAUSE there is already a form that pops up when you click the login button
 })
 
 //vendor sign up route
@@ -43,17 +32,26 @@ router.get("/update-product",(req,res)=>{
     res.render("updateProduct")
 })
 
-//render event by id
+//I dont think we need this because you should only get to update a product IF you are logged in as a vendor...
 router.get("/update-product",(req,res)=>{
     res.render("updateProduct")
 })
 
-// //render vendors by event
-// router.get("/event/vendors",(req,res)=>{
-//     res.render("event")
-// })
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//products by events
+//renders events
+router.get('/',(req,res)=>{
+    Event.findAll({
+    }).then(data=>{
+        const hbsData = data.map(modelIns=>modelIns.toJSON())
+        res.render("home",{
+            events:hbsData
+        })
+    })
+
+})
+
+//find one event's vendors (hoping to add products associated with each vendor)
 router.get("/event/:id",(req,res)=>{
     Event.findByPk(req.params.id,{
         include:[Vendor]
@@ -62,6 +60,38 @@ router.get("/event/:id",(req,res)=>{
         console.log(hbsData)
         res.render("eventPage",hbsData)
     })
+});
+
+//Vendor List: Includes all vendors
+router.get('/vendor',(req,res)=>{
+    Vendor.findAll({
+    }).then(data=>{
+        const hbsData = data.map(modelIns=>modelIns.toJSON())
+        res.render("vendors",{
+            vendors:hbsData
+        })
+    })
+});
+
+//get one Vendor. Shows related products
+
+router.get('/vendor/:id',(req, res)=>{
+    Vendor.findByPk(req.params.id,{
+        include:[Product]
+    }).then(data=>{
+        const hbsData = data.toJSON()
+        console.log(hbsData)
+        res.render("vendorPage",hbsData)
+    })
 })
 
+router.get('/event',(req,res)=>{
+    Event.findAll({
+    }).then(data=>{
+        const hbsData = data.map(modelIns=>modelIns.toJSON())
+        res.render("allEvents",{
+            events:hbsData
+        })
+    })
+});
 module.exports = router;
